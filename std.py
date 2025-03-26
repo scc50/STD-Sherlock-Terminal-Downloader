33f10d2c377ce3b6d49732ddf7b29e04c956f44a
scc50 <sherlockcommandcentre@gmail.com>
2025-03-26 09:38:17 +0200

    Snai Terminal Downloader

--- std.py
+++ std.py
@@ -10,23 +10,24 @@ ___) |  _  | |___|  _ <| |__| |_| | |___| . \    | | | |_| |
 """)
 
 
-import urllib.request
-from rich.progress import Progress
+import urllib.request # Kugirango Dukore HTTP REQUEST
+from rich.progress import Progress # Kwerekana Karia Ga Progress Bar
 
 url = "https://i.pinimg.com/736x/18/a0/ce/18a0ce75262459dc6ecc57233cc8d3b8.jpg"
-file_name = url.split("/").pop()
+file_name = url.split("/").pop() # Kubitandukanya Ukoresheke / Then nkahita delete that part
 
-print(file_name)
 
 with urllib.request.urlopen(url) as data:
-    file_size = int(data.getheader("Content-Length"))
+    file_size = int(data.getheader("Content-Length")) # Kugirango Tubone File Sise
 
 
 with Progress() as progress_bar:
+    # Progress Bar WIth download
     task = progress_bar.add_task(f"[blue]Downloading {file_name}", total=file_size)
 
+    # Call back function ya Ku Downloading no Kwa Updating value ya Progress Bar
     def reporthook(block_num, block_size, total_size):
         progress_bar.update(task, advance=block_size)
 
 
-    urllib.request.urlretrieve(url, file_name, reporthook)
+    urllib.request.urlretrieve(url, file_name, reporthook) # Calling it with callback
